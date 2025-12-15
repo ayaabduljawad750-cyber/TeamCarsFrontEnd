3b971b0b3f6343027b2ca739fdd7233e9cb53a90
ShAhD AhMeD <shahd55ahmed2005@gmail.com>
2025-12-15 16:04:51 +0200

    hh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { AllProductsComponent } from './pages/all-products/all-products.componen
 /* ===================== COMPONENTS ===================== */
 import { OurNavComponent } from './components/our-nav/our-nav.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+// import { FooterComponent } from './components/footer/footer.component';
 
 import { DashboardAdminComponent } from './components/dashboard-admin/dashboard-admin.component';
 import { DashboardUserComponent } from './components/dashboard-user/dashboard-user.component';
@@ -38,8 +38,8 @@ import { CartComponent } from './components/cart/cart.component';
 
 import { CardComponent } from './pages/home/card/card.component';
 import { Section4Component } from './pages/home/section4/section4.component';
-
 import { FooterComponent } from './components/footer/footer.component';
+// import { DashboardMaintenanceCenterComponent } from './components/dashboard-maintenance-center/dashboard-maintenance-center.component';
 import { Section2Component } from './pages/home/section2/section2.component';
 import { ProductCardComponent } from './pages/home/section2/product-card/product-card.component';
 import { SelectFormComponent } from './pages/home/section2/select-form/select-form.component';
@@ -76,7 +76,7 @@ import { SelectFormComponent } from './pages/home/section2/select-form/select-fo
     CartComponent,
 
     CardComponent,
-
+// <<<<<<< HEAD
     DashboardSellerComponent,
     ForgotPasswordComponent,
     Section4Component,
@@ -86,8 +86,9 @@ import { SelectFormComponent } from './pages/home/section2/select-form/select-fo
     ProductCardComponent,
     SelectFormComponent,
     // MyOrdersComponent
+// =======
     Section4Component
-
+// >>>>>>> 153688ed5861b7a491cada6189e5d40fe7318c57
   ],
   imports: [
     BrowserModule,
